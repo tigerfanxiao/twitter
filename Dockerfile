fb0818dd5070eee219b3a4ebd169b25c664763a4
xiao <tigerfanxiao@gmail.com>
2022-10-02 00:12:36 +0200

    install django rest framework

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.10-buster
-RUN /usr/local/bin/python -m pip install --upgrade pip -i https://mirrors.aliyun.com/pypi/simple
+RUN /usr/local/bin/python -m pip install --upgrade pip
 RUN mkdir /code
 WORKDIR /code
 COPY requirements.txt /code/
-RUN pip install -i https://mirrors.aliyun.com/pypi/simple -r requirements.txt
+RUN pip install -r requirements.txt
 COPY . /code/
--- twitter/settings.py
+++ twitter/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,11 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 ROOT_URLCONF = "twitter.urls"
 
@@ -77,7 +83,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "twitter",
-        "HOST": "0.0.0.0",
+        "HOST": "192.168.0.24",
         "PORT": "3306",
         "USER": "root",
         "PASSWORD": "123456",
