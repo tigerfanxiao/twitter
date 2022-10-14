fff45174334ea193a7ad7e518e675a2806cb0333
xiao <tigerfanxiao@gmail.com>
2022-10-14 14:58:29 +0200

    install restframework and create User Viewset

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from accounts.api.serializers import UserSerializer
 
 
@@ -8,6 +10,23 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
+    permission_classes = [permissions.IsAuthenticated]
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+
+
+class AccountViewSet(viewsets.ViewSet):
+    @action(methods=['GET'], detail=False)
+    # method 指定只能用 GET 做请求,
+    # detail=False 表示这不是定义在某个具体 object 上的动作, 而是整体的动作
+    # 如果 detail=True时, 需要在login_status内增加以 pk 变量, 然后需要通过 api/accounts/1/login_status 指定在某个对象上的动作
+    def login_status(self, request):
+        data = {
+            'has_logged_in': request.user.is_authenticated,
+            'ip': request.META['REMOTE_ADDR'],
+            }
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+        return Response(data)
+
--- twitter/urls.py
+++ twitter/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from accounts.api.views import UserViewSet
+from accounts.api.views import UserViewSet, AccountViewSet
 
-router = routers.DefaultRouter()
-router.register(r'api/users', UserViewSet)
+router = routers.DefaultRouter()   # 使用 rest_framework的 router 来注册 url
+router.register(r'api/users', UserViewSet)  # 注册了 api/users页面, 使用 UserViewSet 来处理请求
+router.register(r'api/accounts', AccountViewSet, basename='accounts')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls)),  # 引用 router, 来定义首页
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),  # restframe文档中, 要求配置
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
