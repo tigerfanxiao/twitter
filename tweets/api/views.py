5955cf79b903bf0ef425b00874a43060d9d21c66
xiao <tigerfanxiao@gmail.com>
2022-10-22 08:49:59 +0200

    tweet create api

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from tweets.models import Tweet
-from tweets.api.serializers import TweetSerializer
+from tweets.api.serializers import TweetSerializer, TweetSerializerForCreate
 
 
 # 一般不用 ModelViewSet, 因为 ModelViewSet 默认你增删查改都可以做, 我们不打算开放这些接口
@@ -11,7 +11,8 @@ from tweets.api.serializers import TweetSerializer
 class TweetViewSet(GenericViewSet):
     # 指定默认的 queryset, serializers,
     queryset = Tweet.objects.all()  # 这里可以不需要, 因为 list方法已经返回了一个 queryset
-    serializer_class = TweetSerializer  # 默认的 serializer_class django 会提供一个创建时用的表单
+    serializer_class = TweetSerializerForCreate
+    # 默认的 serializer_class django 会提供一个创建时用的表单
 
 
     # 权限检测有两种方法
@@ -35,6 +36,31 @@ class TweetViewSet(GenericViewSet):
         })  # 一般是 return 一个 dict, 不会直接 return 一个 list
 
 
-
     def create(self, request):
-        pass
+        # 提取用户提交的数据
+        # 初始化一个 serializer, 传入用户输入和 request.user
+        # 初始化 serializer 有基本参数, 通过data传入用户输入, 通过context传入其他参数(以键值对的方式传入)
+        # data 传入的参数在 validated_data中获取, context 传入的参数在 self.context中获取
+        serializer = TweetSerializerForCreate(
+            data=request.data,  # 因为是 post 方法, 所以用户传入数据在 request.data中, 传入的也是键值对
+            context={'request': request}
+        )
+        # 默认都要用 serialzier.is_valid()方法, 对数据进行验证.
+        # 因为这里只是验证 content 的长度, rest_frame会帮我们做, 所以不需要重写 validate 方法
+        # 在方法中如果验证失败, 返回的 VaidationError 信息会填充到 serializer.errors中
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': 'Please check your input',
+                # 这里返回的是一个 list, 即rest_frame帮我们验证的错误, 和我们自己重写 validate 方法返回的错误
+                'errors': serializer.errors,
+            }, status=400)
+        # 调用 serializier.save() 把数据保存下来, 并返回创建的实例
+        # 注意: 这个实例不能直接给 respose 返回, 需要序列化后才能返回
+        tweet = serializer.save()
+        # 返回 Response
+        return Response({
+            'success': True,
+            'tweet': TweetSerializer(tweet).data,  # 这里我们用 TweetSerializer对实例进行序列化
+        }, status=201)  # 创建成功, 返回值 201
+
